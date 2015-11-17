951ff9e7ad16c51c9b6937043e189857eb1ba634
olivialim <olivialim@u.northwestern.edu>
2015-11-17 12:51:35 -0500

    first project commit

---
+++ ProjectBook/ProjectBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectBook
+//
+//  Created by Olivia Lim on 11/17/15.
+//  Copyright © 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectBook/ProjectBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectBook
+//
+//  Created by Olivia Lim on 11/17/15.
+//  Copyright © 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
