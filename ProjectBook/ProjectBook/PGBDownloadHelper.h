2ae4cd7aa264b895894968b1f73bab6c1496186c
olivialim <olivialim@u.northwestern.edu>
2015-11-17 16:28:27 -0500

    added class for download

---
+++ ProjectBook/ProjectBook/PGBDownloadHelper.h
@@ -0,0 +1,13 @@
+//
+//  PGBDownloadHelper.h
+//  ProjectBook
+//
+//  Created by Olivia Lim on 11/17/15.
+//  Copyright © 2015 FIS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGBDownloadHelper : NSObject
+
+@end
---
+++ ProjectBook/ProjectBook/PGBDownloadHelper.m
@@ -0,0 +1,32 @@
+//
+//  PGBDownloadHelper.m
+//  ProjectBook
+//
+//  Created by Olivia Lim on 11/17/15.
+//  Copyright © 2015 FIS. All rights reserved.
+//
+
+#import "PGBDownloadHelper.h"
+
+@implementation PGBDownloadHelper
+
+-(void)download:(NSURL *)url
+{
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    
+    NSURL *URL = [NSURL URLWithString:@"http://www.gutenberg.org/ebooks/50470.epub.images"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+    
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"File downloaded to: %@", filePath);
+    }];
+    [downloadTask resume];
+}
+
+@end
+
+
