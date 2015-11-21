f4f42854369006b5ad03d627904bba6bbcb8d5b1
wojunfeng <wjun1924@gmail.com>
2015-11-20 22:13:40 -0500

    commit before trying out another search bar approach

--- ProjectBook/ProjectBook/PGBMyBookViewController.m
+++ ProjectBook/ProjectBook/PGBMyBookViewController.m
@@ -189,35 +189,8 @@
     [self.myBookListTableView reloadData];
 }
 
-
-//-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
-//    // Tells the table data source to reload when text changes
-//
-////    [self filterContentForSearchText:searchString scope:
-////     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]]];
-//    // Return YES to cause the search result table view to be reloaded.
-//    return YES;
-//}
-//
-//-(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption {
-//    // Tells the table data source to reload when scope bar selection changes
-////    [self filterContentForSearchText:self.searchDisplayController.searchBar.text scope:
-////     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:searchOption]];
-//    // Return YES to cause the search result table view to be reloaded.
-//    return YES;
-//}
-//- (void)didDismissSearchController:(UISearchController *)searchController{
-//    self.bookSearchBar.hidden = YES;
-//}
-//
-//- (void)willDismissSearchController:(UISearchController *)searchController{
-//    self.bookSearchBar.hidden = YES;
-//        NSLog(@"dismiss seach contrlle");
-//}
-//
-//- (void)updateSearchResultsForSearchController:(UISearchController *)searchController{
-//    NSLog(@"update search result!");
-//}
-
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    [self.bookSearchBar resignFirstResponder];
+}
 
 @end
