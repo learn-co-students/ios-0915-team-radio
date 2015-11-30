151c50f0cf413d75a8f9dea193838f6736d4ccc9
laurrenreed <lauren.reed@me.com>
2015-11-30 16:03:28 -0500

    add UI for login page

--- ProjectBook/ProjectBook/PGBLoginViewController.m
+++ ProjectBook/ProjectBook/PGBLoginViewController.m
@@ -34,19 +34,21 @@
     [self.logInView.facebookButton setImage:nil forState:UIControlStateNormal];
     [self.logInView.facebookButton setImage:nil forState:UIControlStateHighlighted];
 
-    [self.logInView.facebookButton setBackgroundImage:[UIImage imageNamed:@"facebook_down.png"] forState:UIControlStateHighlighted];
-    
-    [self.logInView.facebookButton setBackgroundImage:[UIImage imageNamed:@"facebook.png"] forState:UIControlStateNormal];
+    [self.logInView.facebookButton setBackgroundImage:[UIImage imageNamed:@"buttonOutline.png"] forState:UIControlStateHighlighted];
+    [self.logInView.facebookButton setBackgroundImage:[UIImage imageNamed:@"buttonOutline.png"] forState:UIControlStateNormal];
     
     [self.logInView.facebookButton setTitle:@"" forState:UIControlStateNormal];
     [self.logInView.facebookButton setTitle:@"" forState:UIControlStateHighlighted];
     
+    
     [self.logInView.signUpButton setBackgroundImage:[UIImage imageNamed:@"buttonOutline.png"] forState:UIControlStateNormal];
-//    [self.logInView.signUpButton setBackgroundImage:[UIImage imageNamed:@"signup_down.png"] forState:UIControlStateHighlighted];
+    [self.logInView.signUpButton setBackgroundImage:[UIImage imageNamed:@"buttonOutline.png"] forState:UIControlStateHighlighted];
     [self.logInView.signUpButton setTitle:@"" forState:UIControlStateNormal];
     [self.logInView.signUpButton setTitle:@"" forState:UIControlStateHighlighted];
+    
     [self.logInView.logInButton setBackgroundImage:[UIImage imageNamed:@"buttonOutline.png"] forState:UIControlStateNormal];
-           // Remove text shadow
+    
+    // Remove text shadow
     CALayer *layer = self.logInView.usernameField.layer;
     layer.shadowOpacity = 0.0;
     layer = self.logInView.passwordField.layer;
@@ -57,6 +59,9 @@
     [self.logInView.usernameField setBackgroundColor:[UIColor clearColor]];
     [self.logInView.passwordField setTextColor:[UIColor colorWithRed:135.0f/255.0f green:118.0f/255.0f blue:92.0f/255.0f alpha:1.0]];
     [self.logInView.passwordField setBackgroundColor:[UIColor clearColor]];
+    
+    [self.logInView.passwordForgottenButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
 }
 
 - (void)viewDidLayoutSubviews {
@@ -67,19 +72,28 @@
     CGFloat height = screenRect.size.height;
     
     CGFloat leftMargin = width * 1/8;
+    CGFloat rightMargin = width * 3/4;
     
+    CGFloat widthOfBottomButtons = 30.0f;
+
     // CGRectMake(x, y, width, height)
     // Set frame for elements
     
     [self.logInView.dismissButton setFrame:CGRectMake(10.0f, 15.0f, 30.5f, 30.5f)];
+    
     [self.logInView.logo setFrame:CGRectMake(width * 1/5, height * 1/8, width * 3/5, 58.5f)];
-    [self.logInView.facebookButton setFrame:CGRectMake(35.0f, 350.0f, 200.0f, 60.0f)];
-    [self.logInView.signUpButton setFrame:CGRectMake(150.0f, 500.0f, width / 4 , 40.0f)];
-    [self.logInView.usernameField setFrame:CGRectMake(leftMargin, height * 1/4, width * 3/4, 50.0f)];
-    [self.logInView.passwordField setFrame:CGRectMake(leftMargin, height * 1/4 + 50, width * 3/4, 50.0f)];
-    [self.logInView.logInButton setFrame:CGRectMake(leftMargin + 20, height * 1/4 + 125, width * 3/4 - 40, 50.0f)];
-    [self.logInView.passwordForgottenButton setFrame:CGRectMake(width * 1/4, height * 3/4 + 127, 50.0f, 50.0f)];
+    
+    [self.logInView.usernameField setFrame:CGRectMake(leftMargin, height * 1/4, rightMargin, 50.0f)];
+    [self.logInView.passwordField setFrame:CGRectMake(leftMargin, height * 1/4 + 50, rightMargin, 50.0f)];
+    
+    [self.logInView.logInButton setFrame:CGRectMake(leftMargin + 20, height * 1/4 + 124, width * 3/4 - 40, 50.0f)];
+    [self.logInView.passwordForgottenButton setFrame:CGRectMake(width * 1/4, height * 1/4 + 165, 200.0f, 50.0f)];
+    
+    [self.logInView.facebookButton setFrame:CGRectMake(leftMargin, height * 6.9/8 + 38, rightMargin, widthOfBottomButtons)];
+    [self.logInView.signUpButton setFrame:CGRectMake(leftMargin, height * 6.9/8, rightMargin, widthOfBottomButtons)];
+    
     [self.backgroundImageView setFrame: self.logInView.frame];
+
 }
 
 @end
