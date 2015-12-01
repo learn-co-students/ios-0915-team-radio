31d04b23e31b96c92ba1209bdc86ed6d99ef1fbb
wojunfeng <wjun1924@gmail.com>
2015-12-01 14:27:32 -0500

    search working with core data books

--- ProjectBook/ProjectBook/PGBSearchViewController.m
+++ ProjectBook/ProjectBook/PGBSearchViewController.m
@@ -11,6 +11,7 @@
 #import "PGBRealmBook.h"
 #import "PGBDataStore.h"
 #import "Book.h"
+#import "PGBBookPageViewController.h"
 #import <Masonry/Masonry.h>
 
 @interface PGBSearchViewController () <UISearchBarDelegate, UITableViewDelegate,UITableViewDataSource>
@@ -20,14 +21,14 @@
 
 @property (strong, nonatomic) UISearchBar *bookSearchBar;
 @property (strong, nonatomic) UIView *defaultContentView;
-@property (strong, nonatomic) NSArray *books;
-@property (strong, nonatomic) NSArray *booksDisplayed;
+@property (strong, nonatomic) NSMutableArray *books;
 @property (strong, nonatomic) PGBDataStore *dataStore;
 
 @end
 
 @implementation PGBSearchViewController
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
     
     //create search bar
@@ -96,7 +97,15 @@
     
     self.dataStore = [PGBDataStore sharedDataStore];
     [self.dataStore fetchData];
+    
+    self.books = [[NSMutableArray alloc]init];
     //end test data
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    
+    [super viewWillAppear:animated];
+    self.bookSearchBar.hidden = NO;
 }
 
 -(void)fictionButtonTapped:(UIButton *)sender {
@@ -108,6 +117,7 @@
 }
 
 -(void)viewDidAppear:(BOOL)animated{
+    
     [super viewDidAppear:animated];
     
 }
@@ -117,7 +127,7 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.booksDisplayed.count;
+    return self.books.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -126,11 +136,16 @@
         
         PGBBookCustomTableCell *cell = (PGBBookCustomTableCell *)[tableView dequeueReusableCellWithIdentifier:@"CustomCell" forIndexPath:indexPath];
         
-        Book *book = self.booksDisplayed[indexPath.row];
-        
-        cell.titleLabel.text = book.eBookTitles;
-        cell.authorLabel.text = book.eBookAuthors;
-        cell.genreLabel.text = book.eBookGenres;
+        PGBRealmBook *realmBook = self.books[indexPath.row];
+
+        if (realmBook.title.length != 0) {
+            cell.titleLabel.text = realmBook.title;
+        } else {
+            cell.titleLabel.text = realmBook.friendlyTitle;
+        }
+
+        cell.authorLabel.text = realmBook.author;
+        cell.genreLabel.text = realmBook.genre;
         
         return cell;
     }
@@ -138,32 +153,86 @@
     return [[UITableViewCell alloc]init];
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [self.bookSearchBar resignFirstResponder];
+    self.bookSearchBar.hidden = YES;
+    
+    [self performSegueWithIdentifier:@"bookDetailSegue" sender:nil];
+    
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    PGBBookPageViewController *bookPageVC = segue.destinationViewController;
+    
+    NSIndexPath *selectedIndexPath = self.bookTableView.indexPathForSelectedRow;
+    PGBRealmBook *bookAtIndexPath = self.books[selectedIndexPath.row];
+//    Book *bookAtIndexPath = self.books[selectedIndexPath.row];
+    
+    if (bookAtIndexPath.title.length != 0) {
+        bookPageVC.titleBook = bookAtIndexPath.title;
+    } else {
+        bookPageVC.titleBook = bookAtIndexPath.friendlyTitle;
+    }
+    
+    bookPageVC.author = bookAtIndexPath.author;
+    bookPageVC.genre = bookAtIndexPath.genre;
+    bookPageVC.language = bookAtIndexPath.language;
+    bookPageVC.bookDescription = @"No description yet";
+    bookPageVC.ebookID = bookAtIndexPath.ebookID;
+    bookPageVC.books = bookPageVC.books;
+}
+
+
 -(void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    
     self.defaultContentView.hidden = YES;
 }
 
 -(void)searchBarTextDidEndEditing:(UISearchBar *)searchBar {
+    
     if ([self.bookSearchBar.text length] == 0) {
         self.defaultContentView.hidden = NO;
     }
 }
 
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
     [self.bookSearchBar resignFirstResponder];
 }
 
 -(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
-    NSPredicate *searchFilter = [NSPredicate predicateWithFormat:@"eBookTitles CONTAINS[c] %@", searchText];
-    self.booksDisplayed = [self.dataStore.managedBookObjects filteredArrayUsingPredicate:searchFilter];
     
-    [self.bookTableView reloadData];
+        //  this is causing some UI lag/FIX ME
+        NSPredicate *searchFilter = [NSPredicate predicateWithFormat:@"(eBookTitles CONTAINS[c] %@) OR (eBookFriendlyTitles CONTAINS[c] %@)", searchText, searchText];
+        //    NSPredicate *searchFilter = [NSPredicate predicateWithFormat:@"(eBookTitles CONTAINS[c] %@) ", searchText];
+        NSArray *coreDataBooks = [self.dataStore.managedBookObjects filteredArrayUsingPredicate:searchFilter];
+        
+        //convert core data book object into PGBRealmBook object
+        [self.books removeAllObjects];
+        
+        for (Book *coreDataBook in coreDataBooks) {
+            PGBRealmBook *realmBook = [[PGBRealmBook alloc]init];
+            realmBook.title = coreDataBook.eBookTitles;
+            realmBook.friendlyTitle = coreDataBook.eBookFriendlyTitles;
+            realmBook.author = coreDataBook.eBookAuthors;
+            realmBook.genre = coreDataBook.eBookGenres;
+            realmBook.language = coreDataBook.eBookLanguages;
+            realmBook.ebookID  = coreDataBook.eBookNumbers;
+            //not getting book cover images here
+            [self.books addObject:realmBook];
+        }
     
+        [self.bookTableView reloadData];
+
     if ([searchText length] == 0) {
         [self performSelector:@selector(hideKeyboardWithSearchBar:) withObject:self.bookSearchBar afterDelay:0];
     }
 }
 
 - (void)hideKeyboardWithSearchBar:(UISearchBar *)searchBar {
+    
     self.defaultContentView.hidden = NO;
     [searchBar resignFirstResponder];
 }
