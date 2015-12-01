e23e849cbe41003e8665d8f0a93b68ebf6b467db
laurrenreed <lauren.reed@me.com>
2015-12-01 13:34:45 -0500

    add settings page and view controler

---
+++ ProjectBook/ProjectBook/PGBSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PGBSettingsViewController.h
+//  ProjectBook
+//
+//  Created by Lauren Reed on 12/1/15.
+//  Copyright © 2015 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGBSettingsViewController : UIViewController
+
+@end
