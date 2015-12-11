2e95c7f1d610be7006b0113338795ce0707bf7a0
olivialim <olivialim@u.northwestern.edu>
2015-12-11 09:45:25 -0500

    added reviews to new vc

--- ProjectBook/ProjectBook/PGBBookViewController.m
+++ ProjectBook/ProjectBook/PGBBookViewController.m
@@ -13,7 +13,7 @@
 #import <Masonry/Masonry.h>
 #import <YYWebImage/YYWebImage.h>
 
-@interface PGBBookViewController ()
+@interface PGBBookViewController () <UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextView *titleTV;
 @property (weak, nonatomic) IBOutlet UILabel *authorLabel;
@@ -22,6 +22,9 @@
 @property (weak, nonatomic) IBOutlet UITextView *bookDescriptionTV;
 @property (weak, nonatomic) IBOutlet UIView *superContentView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *moreOptions;
+
+@property (strong, nonatomic) NSMutableString *htmlString;
+@property (weak, nonatomic) IBOutlet UIView *webViewContainer;
 
 @end
 
@@ -55,6 +58,9 @@
         if (totalHeight < view.frame.origin.y + view.frame.size.height) totalHeight = view.frame.origin.y + view.frame.size.height;
     
     self.bookDescriptionTV.text = @"";
+    self.bookDescriptionTV.editable = NO;
+    self.bookDescriptionTV.selectable = NO;
+    
     PGBGoodreadsAPIClient *goodreadsAPI = [[PGBGoodreadsAPIClient alloc] init];
     [goodreadsAPI getDescriptionForBookTitle:self.book completion:^(NSString *bookDescription) {
         
@@ -65,6 +71,20 @@
                 self.bookDescriptionTV.text = bookDescription;
             }
         }];
+    }];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    //LEO - this is causing crash when back from book detail
+    [self getReviewswithCompletion:^(BOOL success) {
+        if (success) {
+            NSLog(@"Succed to get description from API call - LEO");
+        } else {
+            NSLog(@"failed to get description from API call - LEO");
+            self.bookDescriptionTV.text = @"There is no description for this book.";
+        }
     }];
 }
 
@@ -108,14 +128,73 @@
 
 
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)getReviewswithCompletion:(void (^)(BOOL))completionBlock
+{
+    [PGBGoodreadsAPIClient getReviewsForBook:self.book completion:^(NSDictionary *reviewDict) {
+        
+        if (reviewDict) {
+            
+            self.htmlString = [reviewDict[@"reviews_widget"] mutableCopy];
+            
+            NSData *htmlData = [self.htmlString dataUsingEncoding:NSUTF8StringEncoding];
+            
+            NSURL *baseURL = [NSURL URLWithString:@"https://www.goodreads.com"];
+            
+            // make / constrain webview
+            
+            CGRect webViewFrame = CGRectMake(0, 0, self.webViewContainer.frame.size.width, self.webViewContainer.frame.size.height);
+            
+            self.webView = [[WKWebView alloc]initWithFrame: webViewFrame];
+            [self.webViewContainer addSubview:self.webView];
+            //                self.webView.translatesAutoresizingMaskIntoConstraints = NO;
+            //
+            //                [self.webView.leftAnchor constraintEqualToAnchor:self.webViewContainer.leftAnchor].active = YES;
+            //                [self.webView.rightAnchor constraintEqualToAnchor:self.webViewContainer.rightAnchor].active = YES;
+            //                [self.webView.topAnchor constraintEqualToAnchor:self.webViewContainer.bottomAnchor].active = YES;
+            //                [self.webView.bottomAnchor constraintEqualToAnchor:self.webViewContainer.bottomAnchor].active = YES;
+            
+            
+            [self.webView loadData:htmlData MIMEType:@"text/html" characterEncodingName:@"utf-8" baseURL:baseURL];
+            
+            self.webView.UIDelegate = self;
+            self.webView.navigationDelegate = self;
+            self.webView.scrollView.delegate = self;
+            
+            //            [self.webView.heightAnchor constraintEqualToConstant:300];
+            //            [self.webViewContainer layoutSubviews];
+            completionBlock(YES);
+        } else {
+            completionBlock(NO);
+        }
+    }];
 }
-*/
+
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    NSLog(@"didFinishNavigation");
+    
+    [webView.scrollView setZoomScale:0.6];
+    [webView.scrollView setContentOffset:CGPointMake(0, 0)];
+    
+    //    [webView.scrollView zoomToRect:CGRectMake(0, 0, 20, 20) animated:YES];
+}
+
+
+
+-(void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation
+{
+    NSLog (@"didCommitNavigation");
+    
+    [webView.scrollView setZoomScale:0.6];
+    [webView.scrollView setContentOffset:CGPointMake(0, 0)];
+}
+
+- (void)dealloc {
+    //    [self.webView setDelegate:nil];
+    
+    self.webView.UIDelegate = nil;
+    self.webView.navigationDelegate = nil;
+    self.webView.scrollView.delegate = nil;
+    [self.webView stopLoading];
+}
 
 @end
