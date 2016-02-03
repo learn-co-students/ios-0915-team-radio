0ca91da42a3cb6cd6ef28156e4d375d2e9f3b547
wleofeng <wleofeng@gmail.com>
2016-02-02 22:36:48 -0500

    refactored tab barVC

--- ProjectBook/ProjectBook/PGBTabBarViewController.m
+++ ProjectBook/ProjectBook/PGBTabBarViewController.m
@@ -16,83 +16,50 @@
 
 @implementation PGBTabBarViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
 
--(void)viewWillAppear:(BOOL)animated {
-    
+- (void)viewWillAppear:(BOOL)animated
+{
     [super viewWillAppear:animated];
 
     [[UITabBarItem appearance] setTitleTextAttributes:@{ NSForegroundColorAttributeName : FlatGreenDark }
                                              forState:UIControlStateNormal];
     
-    UIImage *homeIcon = [IonIcons imageWithIcon:ion_ios_home_outline
+    [self createTabBarIconForItem:0 unselectedIcon:ion_ios_home_outline selectedIcon:ion_ios_home];
+    [self createTabBarIconForItem:1 unselectedIcon:ion_ios_search selectedIcon:ion_ios_search_strong];
+    [self createTabBarIconForItem:2 unselectedIcon:ion_ios_bookmarks_outline selectedIcon:ion_ios_bookmarks];
+    [self createTabBarIconForItem:3 unselectedIcon:ion_ios_chatbubble_outline selectedIcon:ion_ios_chatbubble];
+}
+
+- (void)createTabBarIconForItem:(NSUInteger)itemNumber unselectedIcon:(NSString *)unselectedIcon selectedIcon:(NSString *)selectedIcon
+{
+    UIImage *unselectedImage = [IonIcons imageWithIcon:unselectedIcon
                                       iconColor:FlatGreenDark
                                        iconSize:40.0f
                                       imageSize:CGSizeMake(90.0f, 90.0f)];
     
-    [self.tabBar.items[0] setTitle:nil];
-    [self.tabBar.items[0] setImage:homeIcon];
-    self.tabBar.items[0].imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    [self.tabBar.items[itemNumber] setTitle:nil];
+    [self.tabBar.items[itemNumber] setImage:unselectedImage];
+    self.tabBar.items[itemNumber].imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
     
-    UIImage *selectedHomeIcon = [IonIcons imageWithIcon:ion_ios_home
+    UIImage *selectedImage = [IonIcons imageWithIcon:selectedIcon
                                               iconColor:FlatGreenDark
                                                iconSize:40.0f
                                               imageSize:CGSizeMake(90.0f, 90.0f)];
     
-    self.tabBar.items[0].selectedImage = selectedHomeIcon;
-    
-    
-    UIImage *searchIcon = [IonIcons imageWithIcon:ion_ios_search
-                                        iconColor:FlatGreenDark
-                                         iconSize:40.0f
-                                        imageSize:CGSizeMake(90.0f, 90.0f)];
-    
-    [self.tabBar.items[1] setTitle:nil];
-    [self.tabBar.items[1] setImage:searchIcon];
-    self.tabBar.items[1].imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    
-    UIImage *selectedSearchIcon = [IonIcons imageWithIcon:ion_ios_search_strong
-                                                iconColor:FlatGreenDark
-                                                 iconSize:40.0f
-                                                imageSize:CGSizeMake(90.0f, 90.0f)];
-    
-    self.tabBar.items[1].selectedImage = selectedSearchIcon;
-    
-    UIImage *libraryIcon = [IonIcons imageWithIcon:ion_ios_bookmarks_outline
-                                         iconColor:FlatGreenDark
-                                          iconSize:40.0f
-                                         imageSize:CGSizeMake(90.0f, 90.0f)];
-    [self.tabBar.items[2] setTitle:nil];
-    [self.tabBar.items[2] setImage:libraryIcon];
-    self.tabBar.items[2].imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    
-    
-    UIImage *selectedLibraryIcon = [IonIcons imageWithIcon:ion_ios_bookmarks
-                                                 iconColor:FlatGreenDark
-                                                  iconSize:40.0f
-                                                 imageSize:CGSizeMake(90.0f, 90.0f)];
-    
-    self.tabBar.items[2].selectedImage = selectedLibraryIcon;
-
-    UIImage *chatIcon = [IonIcons imageWithIcon:ion_ios_chatbubble_outline
-                                      iconColor:FlatGreenDark
-                                       iconSize:40.0f
-                                      imageSize:CGSizeMake(90.0f, 90.0f)];
-    [self.tabBar.items[3] setTitle:nil];
-    [self.tabBar.items[3] setImage:chatIcon];
-    self.tabBar.items[3].imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-    
-    UIImage *selectedChatIcon = [IonIcons imageWithIcon:ion_ios_chatbubble iconColor:FlatGreenDark iconSize:40.0f imageSize:CGSizeMake(90.0f, 90.0f)];
-    self.tabBar.items[3].selectedImage = selectedChatIcon;
-    
+    self.tabBar.items[itemNumber].selectedImage = selectedImage;
 }
+
+
 @end
